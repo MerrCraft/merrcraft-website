cc6def7992cbe6bdd93cfbc5e6437a96277e0983
MerrCraft <MerrCraft13@gmail.com>
2026-07-29 12:15:20 -0500

    simple changes, better site

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -10,20 +10,20 @@ export default function Home() {
       <div class="sections">
         
       <section>
-        <a class="title">Guestbook</a>
+        <p class="title">Guestbook</p>
         <iframe src="https://merrcraft.atabook.org/" width="100%" height="400px" style="border:0;"></iframe>
       </section>
         
 
       <section>
-        <a class="title">About (simplified)</a>
+        <p class="title">About (simplified)</p>
         Hewwo! I'm MerrCraft, A person intrested in programming and touching grass.<br/>
         As you can tell, I really love cats.<br/>
         if you want more info, go to <a href="/about">about.</a>
       </section>
       
       <section>
-        <a class="title">BOG</a>
+        <p class="title">BOG</p>
         THX BOG FOR CODING INSPIRATION!<br/>
         CHECK OUT HIS PAGE RIGHT NOW OR ELSE<br/>
         <a href="https://bog.dragoncat.org"><img src="https://bog.dragoncat.org/88x31.png" alt="Boggiverse"/></a><br/>
